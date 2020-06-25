9466056dc4306f2ea697dbcd064328ec4498d1e9
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-06-25 21:00:27 +0300

    modules for node.js

---
+++ modules/user/index.js
@@ -0,0 +1,13 @@
+let phrases = require('./ru.json')
+
+function User(name) {
+    this.name = name;
+}
+
+User.prototype.hello = function(who) {
+    console.log(`${phrases.Hello} ${who.name}`);
+}
+
+console.log('User.js is required');
+
+exports.User = User;
