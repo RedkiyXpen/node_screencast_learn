e9e747cfc80f89b72a1531f0306628e741b1d5e2
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-07-07 05:27:42 +0300

    echo server nodejs

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
-let http = require('http');
-let server = new http.Server();
-let emit = server.emit;
-let counter = 0;
+let http = require('http'),
+    url = require('url');
+
+let server = new http.Server((req, res) => {
+    let urlParsed = url.parse(req.url, true);
+    console.log(req.method, req.url);
+    console.log(urlParsed);
+    console.log(req.headers);
+
+    if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
+        res.setHeader('Cache-control', 'no-cache');
+        res.end(urlParsed.query.message);
+    } else {
+        res.statusCode = 404
+        res.end("Page not found");
+    }
+});
 
 server.listen(1337, '127.0.0.1');
-
-server.emit = (event) => {
-    console.log(event);
-    emit.apply(server, arguments);
-}
-
-server.on('request', (req, res) => {
-    res.end("Hello, World!" + ++counter);
-})
