c2589bc3dd0eb6e47929bb85b98486c7fb853022
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-07-08 04:28:27 +0300

    supervisor

--- server/server.js
+++ server/server.js
@@ -1,19 +1,18 @@
 let http = require('http'),
     url = require('url');
 
-let server = new http.Server((req, res) => {
-    let urlParsed = url.parse(req.url, true);
-    console.log(req.method, req.url);
-    console.log(urlParsed);
-    console.log(req.headers);
+let server = http.createServer();
 
-    if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
-        res.setHeader('Cache-control', 'no-cache');
-        res.end(urlParsed.query.message);
-    } else {
-        res.statusCode = 404
-        res.end("Page not found");
+server.on('request', (req, res) => {
+    let urlParsed = url.parse(req.url, true);
+    
+    if (req.method = 'GET' && urlParsed.pathname == '/echo' && urlParsed.query.method) {
+        res.end(urlParsed.query.method);
+        return;
     }
+
+    res.statusCode = 404;
+    res.end('Not Found');
 });
 
 server.listen(1337, '127.0.0.1');
